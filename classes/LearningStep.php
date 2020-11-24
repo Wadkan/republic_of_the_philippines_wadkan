71f47ce7e9def07e647e951de79154f57d672344
wadkan <zsoltgaramszegi@gmail.com>
2020-11-24 13:47:52 +0100

    bugfixed. no error shows....

--- classes/LearningStep.php
+++ classes/LearningStep.php
@@ -38,35 +38,30 @@ class LearningStep
         return $stmt->fetch();
     }
 
-    function save($array_values): int
+    function save($values): int
     {
         $pdo = $this->get_pdo();
         $sql = 'INSERT INTO learning_step (id, topic, is_learned, step, created, last_updated)
             VALUES (:id, :topic, :is_learned, :step, :created, :last_updated)';
         $stmt = $pdo->prepare($sql);
-        $stmt->execute(['id' => $array_values['id'], 'topic' => $array_values['topic'], 'is_learned' => $array_values['is_learned'], 'step' => $array_values['step'], 'created' => $array_values['created'], 'last_updated' => $array_values['last_updated']]);
-
-        if (!$stmt) {
-            echo "\nPDO::errorInfo():\n";
-            print_r($pdo->errorInfo());
-        }
-
+        $stmt->execute(['id' => $values['id'], 'topic' => $values['topic'], 'is_learned' => $values['is_learned'], 'step' => $values['step'], 'created' => $values['created'], 'last_updated' => $values['last_updated']]);
         return $this->get_last_id()['ID'];
     }
+
 }
 
 
 function add_new()
 {
     $next_id = LearningStep::get_instance()->get_last_id()['ID'] + 1;
-    $array_values['id'] = $next_id;
-    $array_values['topic'] = 'asdf';
-    $array_values['is_learned'] = TRUE;
-    $array_values['step'] = 999;
-    $array_values['created'] = "2020-11-11 10:10:10";
-    $array_values['last_updated'] = "2020-11-11 10:10:10";
+    $values['id'] = $next_id;
+    $values['topic'] = 'alma';
+    $values['is_learned'] = TRUE;
+    $values['step'] = 999;
+    $values['created'] = "2020-11-11 10:10:10";
+    $values['last_updated'] = "2020-11-11 10:10:10";
 
-    echo LearningStep::get_instance()->save($array_values);
+    echo LearningStep::get_instance()->save($values);
 }
 
 echo add_new();
